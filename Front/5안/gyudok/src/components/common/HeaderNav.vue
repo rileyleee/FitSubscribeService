b21cb09846388e442ae5d39807cf3875571bdc9e
이은경 <eunclid0202@gmail.com>
2023-05-24 23:53:44 +0900

    230524 css added

--- Front/5안/gyudok/src/components/common/HeaderNav.vue
+++ Front/5안/gyudok/src/components/common/HeaderNav.vue
@@ -1,21 +1,19 @@
 <template>
-  <header>
-    <nav>
+  <header class="shadow mb-5 bg-body-tertiary rounded">
+    <nav class="header-nav">
       <div>
-        헤더입니다.
-        <router-link :to="{ name: 'mainintro' }"> 구독 홈 </router-link>
+        <router-link :to="{ name: 'mainintro' }">
+          <img class="home" src="@/assets/main.png" />
+        </router-link>
       </div>
       <div>
-        <p>
-          <!--부모 이름은 쓸 필요 없고 자식 이름으로 들어가기-->
-          <router-link :to="{ name: 'fac-search' }">
-            운동시설 구독
-          </router-link>
-          <router-link :to="{ name: 'boardList' }">커뮤니티</router-link>
-          <a href="#" v-if="getUser" @click="logout"> 로그아웃 </a>
-          <router-link :to="{ name: 'login' }" v-else> 로그인 </router-link>
-          <router-link :to="{ name: 'signup' }"> 회원가입 </router-link>
-        </p>
+        <router-link :to="{ name: 'fac-search' }"> 운동시설 구독 </router-link>
+        <router-link :to="{ name: 'boardList' }">커뮤니티</router-link>
+      </div>
+      <div>
+        <a href="#" v-if="getUser" @click="logout">로그아웃 </a>
+        <router-link :to="{ name: 'login' }" v-else>로그인 </router-link>
+        <router-link :to="{ name: 'signup' }">회원 가입 </router-link>
       </div>
     </nav>
   </header>
@@ -44,4 +42,41 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@font-face {
+  font-family: "jua";
+  src: url("@/assets/fonts/BMJUA_ttf.ttf");
+}
+div {
+  font-family: "jua";
+  font-size: larger;
+}
+header {
+  height: 70px;
+  background-color: white;
+  line-height: 70px;
+  padding: 0px 30px;
+}
+header a {
+  margin: 10px;
+  text-decoration: none;
+  color: rgb(12, 12, 12);
+}
+
+.header-nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.header-mi .logo {
+  display: inline-block;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  margin: 0;
+}
+.home {
+  width: 170px;
+  height: 65px;
+}
+</style>
